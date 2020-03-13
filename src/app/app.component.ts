8bb21141b815ec2204f4eaed71575270fa119c25
Julie Anne Moore <julieanne.moore@introhive.com>
2020-03-13 10:58:09 -0300

    Made the pattern name selectable and calculate the results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,30 @@ import {PatternService} from './pattern.service';
 
 export class AppComponent {
   title = 'design-pattern-matching';
+  selectedOption: number[] = [];
+  result: string;
+
+  submitted = false;
+
+  submit(): void{
+
+    let correct = 0;
+
+    for(var i = 0; i< this.patterns.length; i++) { 
+      if (this.patterns[i].id == this.selectedOption[i]){
+        correct++;
+        console.log(this.patterns[i].name + " is correct!")
+      } 
+    }
+
+    this.result = "You got " + correct + "/" + this.patterns.length + " right.";
+
+    console.log(this.result)
+
+    if (!this.submitted){
+      this.submitted = true;
+    }
+  }
 
   patterns = []
 
--- src/app/pattern.service.ts
+++ src/app/pattern.service.ts
@@ -7,21 +7,27 @@ import { Injectable } from '@angular/core';
 export class PatternService {
   items: Pattern[] = [
     {
+      id: 0,
       name: "Template Method",
       description: "Subclasses decide how to implement steps in an algorithm"
     },{
+      id: 1,
       name: "Strategy",
       description: "Encapsulate interchangable behaviour and use delegation to descide which behaviour to use"
     }, {
+      id: 2,
       name: "Factory Method",
       description: "Subclasses decide which concrete classes to create"
     }, {
+      id: 3,
       name: "Decorator",
       description: "Doesn't Alter the interface, but adds responsibility"
     }, {
+      id: 4,
       name: "Adapter",
       description: "Converts one interface to another"
     }, {
+      id: 5,
       name: "Facade",
       description: "Makes the interface simpler"
     }
@@ -33,6 +39,7 @@ export class PatternService {
 }
 
 export interface Pattern {
-  name : String;
+  id: Number;
+  name: String;
   description: String;
 }
