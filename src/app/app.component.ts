6bec6a744bf42d846a45458126efc8b4a2cb089b
Julie Anne Moore <julieanne.moore@introhive.com>
2020-03-13 12:55:50 -0300

    add logic to show the right and wrong choices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,59 @@ import {PatternService} from './pattern.service';
 
 export class AppComponent {
   title = 'design-pattern-matching';
-  selectedOption: number[] = [];
+  selectedOption = [];
+  submittedOption = [];
   result: string;
 
   submitted = false;
 
+  wrong(idx: number): string{
+
+
+    if(this.selectedOption[idx] == -1 || this.submittedOption[idx] != this.selectedOption[idx] ||
+        (this.submittedOption[idx] == this.selectedOption[idx] &&
+         this.selectedOption[idx] == idx)){
+      return "hide";
+    } 
+
+    return "";
+  }
+
+  correct(idx: number): string{
+
+    if(this.submittedOption[idx] == this.selectedOption[idx] &&
+       this.selectedOption[idx] == idx){
+      return "";
+    } 
+
+    return "hide";
+  }
+
+  disableSubmit(): boolean{
+    return this.selectedOption.includes("-1");
+  }
+
   submit(): void{
+    if (!this.submitted){
+      this.submitted = true;
+    }
 
     let correct = 0;
 
     for(var i = 0; i< this.patterns.length; i++) { 
+      this.submittedOption[i] = this.selectedOption[i];
+
       if (this.patterns[i].id == this.selectedOption[i]){
         correct++;
         console.log(this.patterns[i].name + " is correct!")
       } 
+
+
     }
 
     this.result = "You got " + correct + "/" + this.patterns.length + " right.";
 
     console.log(this.result)
-
-    if (!this.submitted){
-      this.submitted = true;
-    }
   }
 
   patterns = []
@@ -39,7 +69,10 @@ export class AppComponent {
   constructor(
     private patternService: PatternService
   ) { 
-    this.patterns = patternService.items
+    this.patterns = patternService.items;
+    for(var i = 0; i< this.patterns.length; i++) {
+      this.selectedOption[i] = "-1";
+    }
   }
   
 }
